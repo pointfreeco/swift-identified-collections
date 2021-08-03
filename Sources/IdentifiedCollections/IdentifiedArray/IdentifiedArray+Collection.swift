de3d2a2a32f48334fbf2ab817c81a2152240909b
Stephen Celis <stephen@stephencelis.com>
2021-08-03 15:01:34 -0400

    Remove deprecated index subscript setter (#15)

--- Sources/IdentifiedCollections/IdentifiedArray/IdentifiedArray+Collection.swift
+++ Sources/IdentifiedCollections/IdentifiedArray/IdentifiedArray+Collection.swift
@@ -15,10 +15,6 @@ extension IdentifiedArray: Collection {
   @inline(__always)
   public subscript(position: Int) -> Element {
     _read { yield self._dictionary[offset: position].value }
-    @available(
-      *, unavailable, message: "use the id-based subscript, instead, for in-place modification"
-    )
-    set { fatalError() }
   }
 
   /// Returns a new array containing the elements of the array that satisfy the given predicate.
