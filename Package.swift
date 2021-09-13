c8e6a40209650ab619853cd4ce89a0aa51792754
Stephen Celis <stephen@stephencelis.com>
2021-09-13 17:30:02 -0400

    Update Package.swift (#23)

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/apple/swift-collections", from: "0.0.1"),
+    .package(url: "https://github.com/apple/swift-collections", from: "1.0.0"),
     .package(url: "https://github.com/apple/swift-collections-benchmark", from: "0.0.2"),
   ],
   targets: [
