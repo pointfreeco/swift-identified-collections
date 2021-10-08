dd7402f785100ed9be20a91af233281287b700d3
Steve Roche <sroche@gmail.com>
2021-10-08 12:25:06 -0700

    Remove the comparison of id (KeyPath) in IdentifiedArray Equatable implementation (#25)
    Resolves issue at:
    https://github.com/pointfreeco/swift-identified-collections/issues/24#issuecomment-938697095

--- Sources/IdentifiedCollections/IdentifiedArray/IdentifiedArray+Equatable.swift
+++ Sources/IdentifiedCollections/IdentifiedArray/IdentifiedArray+Equatable.swift
@@ -1,6 +1,6 @@
 extension IdentifiedArray: Equatable where Element: Equatable {
   @inlinable
   public static func == (lhs: Self, rhs: Self) -> Bool {
-    lhs.id == rhs.id && lhs.elementsEqual(rhs)
+    lhs.elementsEqual(rhs)
   }
 }
