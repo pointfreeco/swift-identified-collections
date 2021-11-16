043891094882235b4f577c5119009fc6bbee0229
Kai Oelfke <KaiOelfke@users.noreply.github.com>
2021-11-16 22:47:16 +0100

    Update swift-collections dependency to 1.0.2 (#32)

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/apple/swift-collections", from: "1.0.0"),
+    .package(url: "https://github.com/apple/swift-collections", from: "1.0.2"),
     .package(url: "https://github.com/apple/swift-collections-benchmark", from: "0.0.2"),
   ],
   targets: [
