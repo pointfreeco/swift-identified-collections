00564e40951a1ecf87444545d9e5501b7eb1596c
Stephen Celis <stephen@stephencelis.com>
2022-01-14 09:54:15 -0500

    wip

--- Package.swift
+++ Package.swift
@@ -4,12 +4,6 @@ import PackageDescription
 
 let package = Package(
   name: "swift-identified-collections",
-  platforms: [
-    .iOS(.v13),
-    .macOS(.v10_15),
-    .tvOS(.v13),
-    .watchOS(.v6),
-  ],
   products: [
     .library(
       name: "IdentifiedCollections",
