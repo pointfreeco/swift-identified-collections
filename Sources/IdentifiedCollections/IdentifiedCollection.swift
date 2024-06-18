00141778b8e74a92c5d92b4edcc31e15d42dbc13
Stephen Celis <stephen@stephencelis.com>
2024-06-18 14:14:15 -0700

    wip

--- Sources/IdentifiedCollections/IdentifiedCollection.swift
+++ Sources/IdentifiedCollections/IdentifiedCollection.swift
@@ -1,13 +1,21 @@
+/// A collection of elements that can be uniquely identified.
 public protocol IdentifiedCollection<ID, Element>: Collection {
+  /// A type that uniquely identifies elements in the collection.
   associatedtype ID: Hashable
 
+  /// A type that describes all of the ids in the collection.
   associatedtype IDs: Collection<ID>
 
+  /// A collection of ids associated with elements in the collection.
+  ///
+  /// This collection must contain elements equal to `map(\.id)`.
   var ids: IDs { get }
 
+  /// Accesses the value associated with the given id for reading.
   subscript(id id: ID) -> Element? { get }
 }
 
 public protocol MutableIdentifiedCollection<ID, Element>: IdentifiedCollection {
+  /// Accesses the value associated with the given id for reading.
   subscript(id id: ID) -> Element? { get set }
 }
